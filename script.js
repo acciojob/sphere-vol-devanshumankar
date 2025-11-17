b11b0a2c69b99927ef4d36a38d22c42e9a14f87f
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-11-17 17:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function volume_sphere() {
     //Write your code here
+	const btn=document.querySelector("[type=submit]");
+	btn.addEventListener("click",(e)=>{
+		e.preventDefault();
+		const radius=parseInt(document.querySelector("[name=radius]").value);
+		const volume=document.querySelector("[name=volume]");
+		
+		if(radius<0){
+			volume.value="NaN";
+		}else{
+			v=(4/3)*(22/7*Math.pow(radius,3));
+			volume.value=v.toFixed(4);
+		}
+	})
   
 } 
 
